7e5f63fb87a95f5a38b66db291387efa45c28da0
manellkkk <narcisoemanuelsilvadeamorim20@gmail.com>
2025-10-02 11:07:42 -0300

    Rota de Usuário

--- Front-End/frontend/src/routes/AppRoutes.tsx
+++ Front-End/frontend/src/routes/AppRoutes.tsx
@@ -2,11 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login/LoginUsuário/Login";
 import Main from "../pages/Main/Main";
 
+//Cadastros
+import Usuarios from "../pages/Cadastros/Listagem/ListUsuários/ListaUsuarios";
+
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/usuarios" element={<Usuarios />} />
     </Routes>
   );
 }
