9ddc87bbd618867e120d2acf8b565dadad380df5
manellkkk <narcisoemanuelsilvadeamorim20@gmail.com>
2025-10-05 14:33:54 -0300

    Componente Modal (Pop-Up)

---
+++ Front-End/frontend/src/components/modal/Modal.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Modal.module.css";
+
+interface ModalProps {
+  title?: string;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({ title, onClose, children }: ModalProps) {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <h2>{title}</h2>
+          <button className={styles.closeButton} onClick={onClose}>
+            ×
+          </button>
+        </div>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </div>
+  );
+}
